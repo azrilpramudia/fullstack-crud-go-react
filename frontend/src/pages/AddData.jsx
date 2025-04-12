f16715bff96c84d9e177c2192457e3cf4b483b48
Azril Pramudia <azrilpramudia01@gmail.com>
2025-04-12 22:13:08 +0700

    add data

---
+++ frontend/src/pages/AddData.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+
+const AddData = () => {
+  return (
+    <div>
+      <h1>haii</h1>
+    </div>
+  );
+};
+
+export default AddData;
---
+++ frontend/src/pages/ViewData.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+
+const ViewData = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default ViewData;
